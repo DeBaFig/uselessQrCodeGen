f85721a9813a20a651c55f0c52e3d5975017f1d5
dbass <dbass@DESKTOP-BFFTJ9M>
2025-05-17 10:26:45 -0300

    Adicionar arquivos de projeto.

---
+++ uselessQrCodeGen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uselessQrCodeGen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
